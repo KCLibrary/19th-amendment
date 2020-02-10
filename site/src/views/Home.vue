6be839dac431a09b7bc4f941cb7a555f0d9790b9
Hunter Trammell <39362365+huntertrammell@users.noreply.github.com>
2020-02-10 16:10:56 -0600

    Update 2 10 (#1)
    * completed scaffolding & menu, working on animations & mobile queries then need to tie in CMS
    
    * scaffolded out site, designed most content. working on animations and media queries. will connect with CMS once done

--- site/src/views/Home.vue
+++ site/src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="web-container">
+    <app-navigation/>
+    <app-intro/>
+    <app-history/>
+    <app-partners/>
+    <app-social/>
+    <app-footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import appNavigation from '@/components/Navigation.vue'
+import appIntro from '@/components/Intro.vue'
+import appHistory from '@/components/History.vue'
+import appPartners from '@/components/Partners.vue'
+import appSocial from '@/components/Social.vue'
+import appFooter from '@/components/Footer.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+    appNavigation,
+    appIntro,
+    appHistory,
+    appPartners,
+    appSocial,
+    appFooter
+  },
 }
 </script>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Homemade+Apple|Raleway&display=swap');
+.v-application {
+  font-family: 'Raleway', sans-serif !important;
+}
+.v-application h1.display-4, .v-application h1.display-3, .v-application h1.display-2, .v-application h1.display-1, .cursive-font.display-1, .cursive-font {
+  font-family: 'Homemade Apple', cursive !important;
+}
+.theme--light.v-application .text--primary {color: #339193 !important;} 
+.theme--light.v-application .text--secondary { color: #d5654D !important;}
+.theme--light.v-application .text--accent{color:#e9dfc1 !important;}
+.theme--dark.v-btn.v-btn--icon {
+    color: #339193 !important;
+}
+
+.web-container {
+  background-color: #e9dfc1;
+}
+</style>
